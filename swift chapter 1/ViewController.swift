d79bf40de681aac0d7ec462f0c39e4b435a8ac3b
Srinivas <srinivasvadde882gmail.com>
2017-10-23 18:11:25 +0530

    Initial Commit

---
+++ swift chapter 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift chapter 1
+//
+//  Created by Eminosoft on 23/10/17.
+//  Copyright © 2017 Eminosoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
